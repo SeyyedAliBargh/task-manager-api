390888e60fc27179751d33f627a5c594e4890939
reza mohamadi <rezamohamadish7@gmail.com>
2025-12-22 04:55:41 -0800

    import openapi and get_schema_view in core url

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     'rest_framework_simplejwt',
-    # 'drf_yasg',
+    'drf_yasg',
     # "rest_framework.authtoken",
 ]
 
