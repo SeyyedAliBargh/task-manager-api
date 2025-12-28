c9b704a77f32acac7adf8c0274bfa606a19dab0c
reza mohamadi <rezamohamadish7@gmail.com>
2025-12-27 23:43:21 -0800

    create my projects in view and serializer

--- core/manager/api/v1/serializer.py
+++ core/manager/api/v1/serializer.py
@@ -5,11 +5,28 @@ class SerializerProjects(serializers.ModelSerializer):
     absolute_url = serializers.SerializerMethodField(
         method_name="get_absolute_url"
     )
+    role = serializers.SerializerMethodField(
+        method_name="get_role"
+    )
     class Meta:
         model = Project
-        fields = ('id', 'owner', 'name', 'description', 'created', 'updated', 'status', 'absolute_url')
+        fields = ('id', 'owner', 'name', 'role', 'description', 'created', 'updated', 'status', 'absolute_url')
         read_only_fields = ["id", "owner", "created", "updated"]
 
+
+        def get_role(self, obj):
+            profile = self.context['request'].user.profile
+
+            # اگر صاحب پروژه است
+            if obj.owner_id == profile.id:
+                return 'owner'
+
+            # اگر عضو است
+            membership = getattr(obj, 'current_user_membership', None)
+            if membership:
+                return membership[0].role
+
+            return None
 
     def get_absolute_url(self, obj):
         request = self.context.get("request")
--- core/manager/api/v1/views.py
+++ core/manager/api/v1/views.py
@@ -1,11 +1,10 @@
 from rest_framework import generics
 from manager.api.v1.serializer import SerializerProjects
-from ...models import Project
+from ...models import Project, ProjectMember
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from manager.api.v1.paginations import DefaultPagination
-
-              
+from django.db.models import Q, Prefetch              
 class PublicProjectsAPIView(generics.ListAPIView):
     queryset = Project.objects.filter(status=Project.Visibility.PUBLIC).order_by('-created')
     permission_classes = [AllowAny]
@@ -13,15 +12,39 @@ class PublicProjectsAPIView(generics.ListAPIView):
     serializer_class = SerializerProjects
     
 
+# class CreateProject(generics.GenericAPIView):
+#     serializer_class = SerializerProjects
+#     permission_classes = [IsAuthenticated]
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data = request.data)
+        
+
+
 class MyProjectsAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     pagination_class = DefaultPagination
     serializer_class = SerializerProjects
 
     def get_queryset(self):
-        return Project.objects.filter(
-            owner=self.request.user.profile
+        profile = self.request.user.profile
+        projects = Project.objects.filter(
+            Q(owner = profile)|
+            Q(members__user = profile),
+            members__role__in=[
+                ProjectMember.Role.OWNER,
+                ProjectMember.Role.ADMIN,
+                ProjectMember.Role.MEMBER,
+            ]
+        ).prefetch_related(
+            Prefetch(
+                'members',
+                queryset = ProjectMember.objects.filter(user = profile)
+                .exclude(role = ProjectMember.Role.VIEWER),
+                to_attr = 'current_user_membership'
+            )
         ).distinct()
+
+        return projects
     
 class DetailProjectAPIView(generics.RetrieveAPIView):
     serializer_class = SerializerProjects
